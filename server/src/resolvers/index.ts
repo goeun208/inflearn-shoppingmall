c42e554dfcd5717524f5228eed3cee7128df2878
goeun208 <qop341@gmail.com>
2023-05-19 15:55:36 +0900

    chore

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -1,4 +1,4 @@
-import cartResolver from "./cart";
-import productResolver from "./product";
+import cartResolver from "./cart"
+import productResolver from "./product"
 
 export default [productResolver, cartResolver]
