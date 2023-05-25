65fab93d672531cc2f53e5ab5997c7704598b91d
goeun208 <qop341@gmail.com>
2023-05-25 14:04:31 +0900

    move to file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const { VITE_BASE_URL } = process.env
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/inflearn-shoppingmall/"
+  base: VITE_BASE_URL
 })
