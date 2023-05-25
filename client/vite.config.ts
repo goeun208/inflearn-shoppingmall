9451a1bf6921680a8ff90f38dae66ff2f5430648
goeun208 <qop341@gmail.com>
2023-05-25 14:09:20 +0900

    vite.config.ts 수정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const { VITE_BASE_URL } = process.env
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: VITE_BASE_URL
+  base: "/inflearn-shoppingmall/"
 })
