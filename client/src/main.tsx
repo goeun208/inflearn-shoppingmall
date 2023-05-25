f76308bea7cc89756a52113e2720fb274b273167
goeun208 <qop341@gmail.com>
2023-05-25 10:27:19 +0900

    base url 설정

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,11 +5,12 @@ import './scss/index.scss'
 import App from './app'
 import { RecoilRoot } from 'recoil';
 
+const { VITE_BASE_URL } = import.meta.env
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RecoilRoot>
-      <BrowserRouter>
+      <BrowserRouter basename={VITE_BASE_URL}>
         <App />
       </BrowserRouter>
     </RecoilRoot>
